e28bcd3d56f38b4964a21eb5d600bf4fe44d2b83
John Cook <cooks.johns@gmail.com>
2019-10-28 21:30:01 -0500

    start up drills

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,8 +1,7 @@
+import _ from 'lodash';
+
 function componet() {
 	const element = document.createElement('div');
-
-// Lodash, currently included via a script, 
-//			is required for this line to work 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
 	return element;
